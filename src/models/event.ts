62a35998a6c01a3e726db0376595960b8a3f6cfb
mwenyo <mwenyo@gmail.com>
2022-03-11 17:42:40 -0300

    feat: added user model

---
+++ src/models/event.ts
@@ -0,0 +1,11 @@
+import UserEntity from "../db/entities/user";
+
+export interface EventDTO {
+  id?: string;
+  name?: string;
+  promoter?: UserEntity;
+  address?: string;
+  date?: Date;
+  tickets?: number;
+  limitByParticipant?: boolean;
+}
